5129e44dcd384363d780dae5844bd09e48ec8a76
Marlon Müller <marmueller@united-internet.de>
2022-10-18 09:37:58 +0200

    Improve return types

--- packages/actions-core/src/buildContext.ts
+++ packages/actions-core/src/buildContext.ts
@@ -2,17 +2,17 @@ import { Context } from '@actions/github/lib/context';
 import * as github from '@actions/github';
 
 export default class BuildContext {
-  required(key: keyof Context): string {
+  required<K extends keyof Context>(key: K): Context[K] {
     if (github.context[key] === undefined) {
       throw new Error('Could not get context value ' + key);
     }
-    return github.context[key].toString();
+    return github.context[key];
   }
 
-  optional(key: keyof Context): string | null {
+  optional<K extends keyof Context>(key: K): Context[K] | null {
     if (github.context[key] === undefined) {
       return null;
     }
-    return github.context[key].toString();
+    return github.context[key];
   }
 }
