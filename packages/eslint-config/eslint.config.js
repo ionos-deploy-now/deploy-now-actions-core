5dc2ecd0a190741f62ddd6fe34b368ddd5476859
Marcel Solle <marcel.solle@ionos.com>
2026-05-08 13:34:51 +0200

    Update eslint config

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,45 +1,20 @@
-module.exports = {
-    root: true,
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierRecommended = require('eslint-plugin-prettier/recommended');
 
-    env: {
-        node: true,
-    },
-
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-
+module.exports = [
+  ...tsPlugin.configs['flat/recommended'],
+  {
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? ['error', {allow: ['error']}] : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'prettier/prettier': [
-            'warn',
-            {},
-            {
-                usePrettierrc: true,
-            },
-        ],
-        'no-unused-vars': 'off',
-        'no-empty-pattern': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-inferrable-types': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off'
+      'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error'] }] : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-unused-vars': 'off',
+      'no-empty-pattern': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'none' }],
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
     },
-
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-    },
-
-    overrides: [
-        {
-            files: ['**/__tests__/*.{j,t}s?(x)'],
-            env: {
-                jest: true,
-            },
-        },
-    ],
-};
+  },
+  prettierRecommended,
+];
